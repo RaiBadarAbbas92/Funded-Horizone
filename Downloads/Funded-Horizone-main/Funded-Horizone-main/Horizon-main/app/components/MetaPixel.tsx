0fdef24e66663065ea680780268ce5da53f7ac92
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2025-03-10 02:07:55 -0700

    Update MetaPixel.tsx

--- Downloads/Funded-Horizone-main/Funded-Horizone-main/Horizon-main/app/components/MetaPixel.tsx
+++ Downloads/Funded-Horizone-main/Funded-Horizone-main/Horizon-main/app/components/MetaPixel.tsx
@@ -8,37 +8,33 @@ const MetaPixel = () => {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.fbq =
-        window.fbq ||
-        function () {
-          (window.fbq as any).callMethod
-            ? (window.fbq as any).callMethod.apply(window.fbq, arguments)
-            : (window.fbq as any).queue.push(arguments);
-        };
+      // Initialize Facebook Pixel
+      window.fbq = window.fbq || function() {
+        (window.fbq as any).callMethod ?
+        (window.fbq as any).callMethod.apply(window.fbq, arguments) :
+        (window.fbq as any).queue.push(arguments);
+      };
       if (!(window as any)._fbq) (window as any)._fbq = window.fbq;
       (window.fbq as any).push = (window.fbq as any).push || [];
       (window.fbq as any).loaded = true;
       (window.fbq as any).version = "2.0";
       (window.fbq as any).queue = [];
-      const script = document.createElement("script");
-      script.async = true;
-      script.src = "https://connect.facebook.net/en_US/fbevents.js";
-      document.head.appendChild(script);
 
-      window.fbq("init", "1151820459769302");
+      // Initialize with your new Pixel ID
+      window.fbq("init", "4085608968426739");
       window.fbq("track", "PageView");
     }
   }, []);
 
   useEffect(() => {
-    window.fbq("track", "PageView"); // Track every route change
+    // Track page view on route changes
+    window.fbq?.("track", "PageView");
   }, [pathname]);
 
   return (
     <>
-      {/* Meta Pixel Script */}
       <Script
-        id="facebook-pixel"
+        id="fb-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -50,18 +46,17 @@ const MetaPixel = () => {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1151820459769302');
+            fbq('init', '4085608968426739');
             fbq('track', 'PageView');
           `,
         }}
       />
-      {/* NoScript Fallback */}
       <noscript>
         <img
           height="1"
           width="1"
           style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1151820459769302&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=4085608968426739&ev=PageView&noscript=1"
         />
       </noscript>
     </>
