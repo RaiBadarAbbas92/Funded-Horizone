885c0b199c66097feb322b471703af64a056a30d
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2025-03-10 05:22:40 -0700

    Update MetaPixel.tsx

--- Downloads/Funded-Horizone-main/Funded-Horizone-main/Horizon-main/app/components/MetaPixel.tsx
+++ Downloads/Funded-Horizone-main/Funded-Horizone-main/Horizon-main/app/components/MetaPixel.tsx
@@ -8,33 +8,37 @@ const MetaPixel = () => {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // Initialize Facebook Pixel
-      window.fbq = window.fbq || function() {
-        (window.fbq as any).callMethod ?
-        (window.fbq as any).callMethod.apply(window.fbq, arguments) :
-        (window.fbq as any).queue.push(arguments);
-      };
+      window.fbq =
+        window.fbq ||
+        function () {
+          (window.fbq as any).callMethod
+            ? (window.fbq as any).callMethod.apply(window.fbq, arguments)
+            : (window.fbq as any).queue.push(arguments);
+        };
       if (!(window as any)._fbq) (window as any)._fbq = window.fbq;
       (window.fbq as any).push = (window.fbq as any).push || [];
       (window.fbq as any).loaded = true;
       (window.fbq as any).version = "2.0";
       (window.fbq as any).queue = [];
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://connect.facebook.net/en_US/fbevents.js";
+      document.head.appendChild(script);
 
-      // Initialize with your new Pixel ID
       window.fbq("init", "4085608968426739");
       window.fbq("track", "PageView");
     }
   }, []);
 
   useEffect(() => {
-    // Track page view on route changes
-    window.fbq?.("track", "PageView");
+    window.fbq("track", "PageView"); // Track every route change
   }, [pathname]);
 
   return (
     <>
+      {/* Meta Pixel Script */}
       <Script
-        id="fb-pixel"
+        id="facebook-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -51,6 +55,7 @@ const MetaPixel = () => {
           `,
         }}
       />
+      {/* NoScript Fallback */}
       <noscript>
         <img
           height="1"
